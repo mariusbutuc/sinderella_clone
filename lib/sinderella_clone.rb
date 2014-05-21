43349572f72269546bccba4bff9cf8c15471b72c
Marius Butuc <marius.butuc@gmail.com>
2014-05-21 19:16:19 -0400

    :green_heart: Green state: all features have been implemented
    Ready for refactoring

--- lib/sinderella_clone.rb
+++ lib/sinderella_clone.rb
@@ -1,6 +1,50 @@
 require 'sinderella_clone/version'
 require 'sinderella_clone/data_store'
+require 'crimp'
 
 module SinderellaClone
-  # Your code goes here...
+  def self.transforms(data, till_midnight = 60)
+    identifier  = Crimp.signature data
+    cloned_data = deep_copy data
+    transformed = yield cloned_data
+
+    store(
+      id:           identifier,
+      original:     data,
+      transformed:  transformed
+    )
+
+    check(identifier, till_midnight)
+
+    identifier
+  end
+
+  def self.get(id)
+    DataStore.instance.get(id)[:transformed]
+  end
+
+  def self.midnight(id)
+    reset_data_at id
+  end
+
+  private
+
+  def self.store(data)
+    DataStore.instance.set data
+  end
+
+  def self.deep_copy(object)
+    Marshal.load(Marshal.dump(object))
+  end
+
+  def self.check(id, seconds)
+    Thread.new do
+      sleep seconds
+      reset_data_at id
+    end
+  end
+
+  def self.reset_data_at(id)
+    DataStore.instance.reset id
+  end
 end
--- lib/sinderella_clone/data_store.rb
+++ lib/sinderella_clone/data_store.rb
@@ -2,4 +2,33 @@ require 'singleton'
 
 class DataStore
   include Singleton
+
+  def set(data)
+    hash_data = {
+      original:     data[:original],
+      transformed:  data[:transformed]
+    }
+
+    container.store(data[:id], hash_data)
+  end
+
+  def get(id)
+    container.fetch(id)
+  end
+
+  def reset(id)
+    original = container.fetch(id)[:original]
+
+    set(
+      id:           id,
+      original:     original,
+      transformed:  original
+    )
+  end
+
+  private
+
+  def container
+    @store ||= {}
+  end
 end
