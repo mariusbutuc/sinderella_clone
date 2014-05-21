920d8845a62f5ef8c028ba00ab66bfa59574cdbb
Marius Butuc <marius.butuc@gmail.com>
2014-05-21 14:04:30 -0400

    Get BDD/TDD-ready
    RSpec, Guard etc.

---
+++ spec/sinderella_clone_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe SinderellaClone do
+  xit 'does stuff'
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,2 @@
+require 'pry'
+require "sinderella_clone" # the Hound should really bark on this one; and just to make sure, let's breach our favourite 80 characters line limit policy.
